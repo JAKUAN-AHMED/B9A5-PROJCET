5e77340fded68d6a5ce6ab154e8f076dab2b01b2
JAKUAN AHMED <nexxerjm111@gmail.com>
2024-02-19 03:20:24 +0600

    added js

--- scripts/javascripts.js
+++ scripts/javascripts.js
@@ -5,6 +5,10 @@ for(const btn of AllBtn)
     btn.addEventListener('click',function handler(event)
     {
         count+=1;
+        const section=event.target.innerText;
+        const seatType="economy";
+        const price=500;
+        showClassSeat('appendSide',section,seatType,price);
         if(count<=4)
         {
             setColor(event);
@@ -22,6 +26,16 @@ for(const btn of AllBtn)
         availableSeat('setAvailable');
         const increaseSeat=document.getElementById('seatTwist');
         increaseSeat.innerText=count;
+        calTotal('total-cost',price);
+        // need discount also which is 15 and its has coupon code if matched
+        GrandTotal('g-total',price);
+
+        /*
+            step-1=>if once visit a button make it true
+            step-2=>make button disabled if once used
+            event.target.setAttribute("disabled", true);
+        */
+
 
         
 
--- scripts/utility.js
+++ scripts/utility.js
@@ -11,9 +11,34 @@ function availableSeat(elementId)
     const presentSeat=AvailableSeat.innerText;
     document.getElementById(elementId).innerText=parseInt(presentSeat)-1;
 }
-// creat li and append some text inside it
-function showClassSeat()
+// create li and append some text inside it
+function showClassSeat(elementId,section,seatType,price)
 {
-    const element=document.getElementById('appendSide');
+    const element=document.getElementById(elementId);
+    const div=document.createElement('div');
+    div.classList.add('grid');
+    div.classList.add('grid-cols-3');
+    const p=document.createElement('p');
+    p.innerText=section;
+    const p2=document.createElement('p');
+    p2.innerText=seatType;
+    const p3=document.createElement('p');
+    p3.innerText=price;
+    div.appendChild(p);
+    div.appendChild(p2);
+    div.appendChild(p3);
+    element.appendChild(div);
 
+}
+function calTotal(elementId,price)
+{
+    const total=document.getElementById(elementId);
+    let cost=parseInt(total.innerText)+parseInt(price);
+    document.getElementById(elementId).innerText=parseInt(cost);
+}
+function GrandTotal(elementId,price)
+{
+    const element=document.getElementById(elementId).innerText;
+    let elementCost=parseInt(element)+parseInt(price);
+    document.getElementById(elementId).innerText=parseInt(elementCost);
 }
